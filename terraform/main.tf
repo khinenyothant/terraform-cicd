f365e0d38592fac782372473ec30158065137808
CNKYOTO\khaine_nyo_thant <khaine_nyo_thant@infonic.co.jp>
2024-05-23 09:29:03 +0900

    testing commit to dev

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+
+  required_version = ">= 1.7.5"
+
+  backend "s3" {
+    bucket         = var.backet_name
+    key            = "terraform.tfstate"
+    region         = var.region
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+# Create EC2 instance
+resource "aws_instance" "demo_instance" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  tags = {
+    Name = var.instance_name
+  }
+}
